2f5e10672a468b91c79c057061662221069ac419
Farrukh Subhani <50488401+mfsbo@users.noreply.github.com>
2025-09-18 01:54:37 +0100

    Revert "Add ResponsiveImage component for optimized image handling in posts"
    This reverts commit b0cc616a4745ba73f7fbb79ddf4ece1a5e02d0cb.

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,12 +1,12 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import ResponsiveImage from './components/ResponsiveImage.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => h(DefaultTheme.Layout, null, {}),
-  enhanceApp(ctx: { app: any }) { // vitepress provides app (Vue App instance)
-    ctx.app.component('ResponsiveImage', ResponsiveImage)
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // Customize layout slots if needed
+    })
   }
 }
